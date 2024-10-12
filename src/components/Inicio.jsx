d4de0545a8adafbc56ea2db045c1aa36adfe1311
Joaquín Cancino <cancinojoaquin12@gmail.com>
2024-10-12 19:28:56 -0300

    new home desing

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,22 +3,19 @@ import { ReactTyped } from "react-typed";
 
 export function Inicio() {
   return (
-    <section
-      id="inicio"
-      className="welcome-section bg-[#001]"
-    >
+    <section id="inicio" className="welcome-section bg-[#001]">
       <div className="welcome-content relative text-white text-center h-screen flex flex-col items-center justify-center mt-5 mx-auto">
-        <div className="px-5 adlam-display-family">
-          <h1 className="text-5xl md:text-6xl font-extrabold mb-6 text-white drop-shadow-lg">
+        <div className="px-5 z-20">
+          <h1 className="text-5xl md:text-6xl font-extrabold mb-6 text-white drop-shadow-lg adlam-display-family">
             Páginas Web {" "}
-          <ReactTyped
-          backSpeed={30}
-            strings={[
+            <ReactTyped
+              backSpeed={30}
+              strings={[
                 "para PYMEs en Puerto Varas y Ensenada",
                 "que atraen Clientes",
                 "Atractivas y Funcionales",
                 "Personalizadas y Efectivas",
-                "Accesibles y Optimizadas"
+                "Accesibles y Optimizadas",
               ]}
               typeSpeed={50}
               backDelay={4000}
@@ -29,9 +26,15 @@ export function Inicio() {
             Joaquín Cancino: Programador y Diseñador Web
           </p>
         </div>
-        <div className="cta">
-          <a className="page-scroll p-3" href="#about">
-            <ArrowDown className="size-7 mt-8 animate-bounce" />
+        <div className="flex items-center gap-x-5 gap-y-4 justify-center flex-wrap mt-9">
+            <a className="py-2.5 px-4 justify-center rounded-[10px] font-bold border flex items-center gap-x-2.5 hover:scale-105 transition-transform duration-300 text-white border-[#7610F8] bg-[#7610F8] text-base md:text-xl md:px-6 shadow-button" href="/#planes">
+                Ver Planes
+            </a>
+          <a
+            href="/#about"
+            className="py-2.5 px-4 justify-center rounded-[10px] font-bold border flex items-center gap-x-2.5 hover:scale-105 transition-transform duration-300 bg-transparent text-white border-white text-base md:text-xl md:px-6"
+          >
+            Saber más
           </a>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export function Home() {
       <ScrollTop />
       <main>
         <div className="z-10 w-40 blur-[200px] top-6 h-40 rounded-full bg-[#7610F8] absolute pointer-events-none select-none md:top-32 left-0"></div>
-        <div className="z-10 w-60 blur-[200px] h-60 rounded-full bg-cyan-600 absolute pointer-events-none select-none top-80 md:top-56 right-0"></div>
+        <div className="z-10 w-60 blur-[200px] h-60 rounded-full bg-[#7610F8] absolute pointer-events-none select-none top-80 md:top-56 right-0"></div>
         <Inicio />
         <About />
         <Features />
