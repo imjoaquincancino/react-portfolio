ea7b58ffbe4941a9ba336b212e5fa9eef312582c
Joaquín Cancino <cancinojoaquin12@gmail.com>
2024-12-15 23:11:09 -0300

    new project La Chocolattina

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,5 +1,12 @@
 const proyects = [
   {
+    name: "La Chocolattina",
+    description:
+      "Página web de La Chocolattina (Landing Page). Cuenta con página de inicio, vitrina interactiva de productos, implementación de servicios de Google Maps, Instagram, WhatsApp y más.",
+    url: "https://lachocolattina.cl",
+    img: "/img/projects/mockup-chocolattina.webp",
+  },
+  {
     name: "Vivero Ensenada",
     description:
       "Página web de Vivero Ensenada. Cuenta con página de inicio, cátalogo de productos, contacto, implementación de servicios de Google Maps, Whatsapp y más.",
@@ -9,7 +16,7 @@ const proyects = [
   {
     name: "Donde Pancho",
     description:
-      "Presentación Digital del restaurante Donde Pancho. Con un diseño moderno, profesional y minimalista. Contiene lo esencial para marcar presencia en linea.",
+      "Presentación Digital (Mini Web) del restaurante Donde Pancho. Con un diseño moderno, profesional y minimalista. Contiene lo esencial para marcar presencia en linea.",
     url: "https://dondepanchoensenada.cl",
     img: "/img/projects/mockup-dondepancho.webp",
   }
