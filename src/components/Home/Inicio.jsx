a018f5888cde5b3df6cb6ae601b8ad9e2f968368
Joaquín Cancino <cancinojoaquin12@gmail.com>
2024-12-25 20:36:15 -0300

    feat: update layout and styling in Features and Inicio components

--- src/components/Home/Inicio.jsx
+++ src/components/Home/Inicio.jsx
@@ -4,7 +4,7 @@ export function Inicio() {
   return (
     <section className="flex h-screen items-center justify-center px-4">
       <div className="bg-gradient-to-l from-brand/40 to-brandlight/20 w-3/4 max-w-xl absolute rotate-[-7deg] z-0 blur-[160px] rounded-full h-96 md:h-80"></div>
-      <div className="z-10 mx-auto px-4 lg:px-20 flex flex-col lg:flex-row items-center gap-6 lg:gap-4 xl:gap-6 relative">
+      <div className="z-10 pt-12 mx-auto px-6 lg:px-20 flex flex-col lg:flex-row items-center gap-6 lg:gap-4 xl:gap-6 relative">
           <header className="flex flex-col items-center justify-center lg:items-start">
               <span className="mb-6">
                 <CTAcampaign
@@ -12,7 +12,7 @@ export function Inicio() {
                   text={"Ofertas de Pre-temporada 2025"}
                 />
               </span>
-              <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold text-center lg:text-start mb-6">
+              <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold text-center lg:text-start mb-6">
                 Impulsa tu negocio con{" "}
                 <span className="block">
                   <strong className="bg-clip-text text-transparent bg-gradient-to-r from-brand to-purple-300 font-extrabold">
@@ -23,21 +23,21 @@ export function Inicio() {
               <p className="max-w-lg mb-6 lg:mb-8 text-center lg:text-left text-[#d0dbec] [&>strong]:text-yellow-200">
                 ¡No te quedes fuera esta temporada de verano! Aprovecha nuestro{" "}
                 <strong>
-                  50% de Descuento en TODOS nuestros planes de Diseño Web
+                  50% de descuento en TODOS nuestros planes de Diseño Web
                 </strong>{" "}
                 y obtén tu propia página web a un precio accesible.
               </p>
               <nav className="flex flex-col sm:flex-row gap-8 sm:gap-36">
                 <a
-                  className="rounded-md bg-gradient-to-t from-brand to-brandlight text-2xl font-bold py-2 px-6 hover:scale-105 transition-all duration-300 text-center"
+                  className="rounded-md bg-gradient-to-t from-brand to-brandlight text-xl md:text-2xl font-bold py-2 px-6 hover:scale-105 transition-all duration-300 text-center"
                   href="/planes"
                 >
                   ¡Obtén tu página con -50%!
                 </a>
               </nav>
           </header>
-        <div className="hidden lg:block lg:w-2/3">
-          <img src="/img/mockup-home.webp" alt="" className="w-full h-auto" />
+        <div className="relative lg:w-2/3">
+          <img src="/img/mockup-home.webp" alt="Obtén tu propia página web" className="w-full h-auto" loading="lazy" />
         </div>
       </div>
     </section>
